f899f1c92182bc9e3f9e48a3699d479fdf0f41cc
HassanAyman <hassan3ayman@gmail.com>
2023-12-12 21:25:32 +0200

    Add service call to retrieve users and their albums, ensuring proper response wrapping.

--- app/src/main/java/com/pluto/bostausers/data/UserRepositoryImp.kt
+++ app/src/main/java/com/pluto/bostausers/data/UserRepositoryImp.kt
@@ -1,9 +1,38 @@
 package com.pluto.bostausers.data
 
 import com.pluto.bostausers.domain.UserRepository
+import com.pluto.bostausers.domain.mapper.toAlbum
+import com.pluto.bostausers.domain.mapper.toUserInfo
+import com.pluto.bostausers.domain.models.Album
+import com.pluto.bostausers.domain.models.UserInfo
+import retrofit2.Response
+import java.io.IOException
+import java.net.UnknownHostException
 import javax.inject.Inject
 
 class UserRepositoryImp @Inject constructor(
-    private val userRepository: UserRepository
+    private val userService: UserService
 ): UserRepository{
+    override suspend fun getUsers(): List<UserInfo> {
+        return wrapResponse { userService.getAllUsers() }.map { it.toUserInfo() }
+    }
+
+    override suspend fun getAllUserAlbums(userId: String): List<Album> {
+        return wrapResponse { userService.getAllUserAlbums(userId) }.map { it.toAlbum() }
+    }
+
+    private suspend fun <T> wrapResponse(
+        function: suspend () -> Response<T>
+    ): T{
+        try {
+            val response = function()
+            if (response.isSuccessful) {
+                return response.body() ?: throw Exception("Null data")
+            } else {
+                throw Exception("Something is wrong")
+            }
+        } catch (e: UnknownHostException) {
+            throw Exception("No internet")
+        }
+    }
 }
--- app/src/main/java/com/pluto/bostausers/domain/UserRepository.kt
+++ app/src/main/java/com/pluto/bostausers/domain/UserRepository.kt
@@ -1,4 +1,9 @@
 package com.pluto.bostausers.domain
 
+import com.pluto.bostausers.domain.models.Album
+import com.pluto.bostausers.domain.models.UserInfo
+
 interface UserRepository {
+    suspend fun getUsers(): List<UserInfo>
+    suspend fun getAllUserAlbums(userId: String): List<Album>
 }
