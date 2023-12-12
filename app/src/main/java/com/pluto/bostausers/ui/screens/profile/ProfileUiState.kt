ed6fec829f54b6e19dcbb7d96b7f555a6784c6df
HassanAyman <hassan3ayman@gmail.com>
2023-12-12 22:40:59 +0200

    add mapper from the entities to the uiState

--- app/src/main/java/com/pluto/bostausers/ui/screens/profile/ProfileUiState.kt
+++ app/src/main/java/com/pluto/bostausers/ui/screens/profile/ProfileUiState.kt
@@ -1,5 +1,8 @@
 package com.pluto.bostausers.ui.screens.profile
 
+import com.pluto.bostausers.domain.models.Album
+import com.pluto.bostausers.domain.models.UserInfo
+
 data class ProfileUiState(
     val isLoading: Boolean = true,
     val errorMessage: String? = null,
@@ -16,4 +19,15 @@ data class UserInfoUiState(
 data class AlbumUiState(
     val id: String = "",
     val title: String = ""
+)
+
+fun UserInfo.toUserInfoUiState() = UserInfoUiState(
+    name = this.name,
+    zipCode = this.address?.zipcode ?: "",
+    address = this.address?.run { "$city, $street, $suite" } ?: ""
+)
+
+fun Album.toAlbumUiState() = AlbumUiState(
+    id = this.id,
+    title = this.title
 )
