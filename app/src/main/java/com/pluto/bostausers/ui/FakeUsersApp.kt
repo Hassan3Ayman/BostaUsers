dc71c0fd5c7916cb15ff5c8819b50357f26bd91c
HassanAyman <hassan3ayman@gmail.com>
2023-12-12 18:24:50 +0200

    init navigation between the screens

---
+++ app/src/main/java/com/pluto/bostausers/ui/FakeUsersApp.kt
@@ -0,0 +1,18 @@
+package com.pluto.bostausers.ui
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.compositionLocalOf
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.pluto.bostausers.ui.navigation.AppNavGraph
+
+val LocalNavController =
+    compositionLocalOf<NavHostController> { error("No NavController found!") }
+@Composable
+fun FakeUsersApp() {
+    val navController = rememberNavController()
+    CompositionLocalProvider(LocalNavController provides navController){
+        AppNavGraph()
+    }
+}
---
+++ app/src/main/java/com/pluto/bostausers/ui/navigation/ScreenDestination.kt
@@ -0,0 +1,5 @@
+package com.pluto.bostausers.ui.navigation
+
+object ScreenDestination {
+    const val Profile = "profile_screen"
+}
