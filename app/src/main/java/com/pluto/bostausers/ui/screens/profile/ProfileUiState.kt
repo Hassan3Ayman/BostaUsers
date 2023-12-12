242ac13011b953dee914c414253460cb39467894
HassanAyman <hassan3ayman@gmail.com>
2023-12-12 22:26:26 +0200

    make profileUiState with the UserInfoUiState and AlbumUiState

---
+++ app/src/main/java/com/pluto/bostausers/ui/screens/profile/ProfileUiState.kt
@@ -0,0 +1,19 @@
+package com.pluto.bostausers.ui.screens.profile
+
+data class ProfileUiState(
+    val isLoading: Boolean = true,
+    val errorMessage: String? = null,
+    val userInfo: UserInfoUiState = UserInfoUiState(),
+    val userAlbums: List<AlbumUiState> = emptyList()
+)
+
+data class UserInfoUiState(
+    val name: String = "",
+    val address: String = "",
+    val zipCode: String = ""
+)
+
+data class AlbumUiState(
+    val id: String = "",
+    val title: String = ""
+)
