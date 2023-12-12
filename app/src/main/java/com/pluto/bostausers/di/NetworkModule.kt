a9a47c207be8537a694898d6a5e38711c40eac05
HassanAyman <hassan3ayman@gmail.com>
2023-12-12 15:15:54 +0200

    init UserService and add NetworkModule

---
+++ app/src/main/java/com/pluto/bostausers/di/NetworkModule.kt
@@ -0,0 +1,46 @@
+package com.pluto.bostausers.di
+
+import com.pluto.bostausers.data.UserService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+internal object NetworkModule {
+
+    private const val BASE_URL = "https://jsonplaceholder.typicode.com"
+
+    @Provides
+    fun provideUserService(
+        okHttpClient: OkHttpClient,
+        gsonConverterFactory: GsonConverterFactory
+    ): UserService {
+        return provideRetrofit(okHttpClient, gsonConverterFactory)
+            .create(UserService::class.java)
+    }
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(
+        client: OkHttpClient,
+        gsonConverterFactory: GsonConverterFactory,
+    ): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(client)
+            .addConverterFactory(gsonConverterFactory)
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideGsonConverterFactory(): GsonConverterFactory {
+        return GsonConverterFactory.create()
+    }
+}
